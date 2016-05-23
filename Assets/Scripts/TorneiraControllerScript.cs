dbafab21220073013026b64ea202d2fb9ec503a0
apolo02 <apolo02@MacBook-Pro-de-Bruno.local>
2016-05-22 21:12:24 -0300

    Dog Love Chase

---
+++ Assets/Scripts/TorneiraControllerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TorneiraControllerScript : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		//anim.Stop();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (Input.GetKeyUp (KeyCode.Space)) 
+		if (Input.GetMouseButtonUp(0))
+		{
+			anim.Stop();
+		}
+	}
+}
